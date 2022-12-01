157b3eb41a1215331019da75dcf1d97c6ae5a461
ajuvercr <arthur.vercruysse@outlook.com>
2022-12-01 06:05:58 +0100

    start 2022 day 1

---
+++ 2022/src/main.rs
@@ -0,0 +1,39 @@
+use std::io::{stdin, BufRead};
+
+fn main() {
+    let mut lines = stdin().lock().lines();
+
+    let mut m1 = 0;
+    let mut m2 = 0;
+    let mut m3 = 0;
+
+    let mut current = 0;
+    while let Some(Ok(line)) = lines.next() {
+        if let Ok(x) = line.parse::<u32>() {
+            current += x;
+        } else {
+            if current > m1 {
+                m3 = m2;
+                m2 = m1;
+                m1 = current;
+            } else if current > m2 {
+                m3 = m2;
+                m2 = current;
+            } else if current > m3 {
+                m3 = current;
+            }
+
+            current = 0;
+        }
+    }
+
+    if current > m1 {
+        m1 = current;
+    } else if current > m2 {
+        m2 = current;
+    } else if current > m3 {
+        m3 = current;
+    }
+
+    println!("{} ", m1 + m2 + m3);
+}
