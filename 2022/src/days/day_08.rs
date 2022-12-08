d1c9973ecec3068120abd7da64a955fbe4c3f587
ajuvercr <arthur.vercruysse@outlook.com>
2022-12-08 11:14:19 +0100

    Day 8 part 2

--- 2022/src/days/day_08.rs
+++ 2022/src/days/day_08.rs
@@ -1,94 +1,138 @@
 use std::io::BufRead;
 
-#[derive(Default)]
-pub struct Row {
-    max: usize,
-    counts: [usize; 11],
+fn create_vec<T: Clone>(size: usize, t: T) -> Vec<T> {
+    let mut out = Vec::with_capacity(size);
+    out.resize(size, t);
+    out
 }
 
-impl Row {
-    fn push(&mut self, el: usize) -> bool {
-        let out = el > self.max;
+#[derive(Default)]
+pub struct Part1 {
+    max: u8,
+}
 
+impl Part1 {
+    fn update(&mut self, index: usize, _: usize, el: u8, row: &mut u128) {
+        let el = el + 1;
+        if el > self.max {
+            *row = *row | 1 << index;
+        }
         self.max = self.max.max(el);
-        self.counts[el] += 1;
-        out
     }
 
-    fn pop(&mut self, el: usize) -> bool {
-        self.counts[el] -= 1;
+    fn clear(&mut self) {
+        self.max = 0;
+    }
+}
 
-        if self.counts[el] == 0 && el == self.max {
-            for i in (0..el).rev() {
-                self.max = i;
-                if self.counts[i] > 0 {
-                    break;
+type ViewDistance = [usize; 10];
+#[derive(Default)]
+pub struct Part2 {
+    vd: ViewDistance,
+}
+
+impl Part2 {
+    fn update(&mut self, index: usize, j: usize, el: u8, row: &mut [usize]) {
+        row[index] = j - self.vd[el as usize];
+        for i in 0..(el + 1) {
+            self.vd[i as usize] = j;
+        }
+    }
+
+    fn clear(&mut self) {
+        self.vd.fill(0);
+    }
+}
+
+macro_rules! f {
+    ($b:ident, $exp:expr) => {
+        if $b {
+            $exp;
+        }
+    };
+}
+pub fn solve<const P1: bool, const P2: bool>(buf: impl BufRead) -> Option<()> {
+    let mut rows: Vec<Vec<u8>> = buf.split(b'\n').flatten().collect();
+    rows.iter_mut()
+        .for_each(|x| x.iter_mut().for_each(|y| *y = *y - b'0'));
+
+    let col: Vec<u8> = create_vec(rows.len(), 0);
+    let mut cols: Vec<Vec<u8>> = create_vec(rows[0].len(), col);
+
+    for (i, x) in rows.iter().enumerate() {
+        for (j, y) in x.iter().enumerate() {
+            cols[j][i] = *y;
+        }
+    }
+
+    let count_row = create_vec(rows.len(), 0);
+    let counts = create_vec(cols.len(), count_row);
+    let mut counts = create_vec(4, counts);
+
+    let inv = rows.len() - 1;
+
+    let mut part1_states = create_vec(4, create_vec(rows.len(), 0));
+    let mut part1 = Part1::default();
+    let mut part2 = Part2::default();
+
+    for (i, row) in rows.iter().enumerate() {
+        for (j, &el) in row.iter().enumerate() {
+            f!(P1, part1.update(j, j, el, &mut part1_states[0][i]));
+            f!(P2, part2.update(j, j, el, &mut counts[0][i]));
+        }
+        f!(P1, part1.clear());
+        f!(P2, part2.clear());
+    }
+
+    for (i, row) in rows.iter().enumerate() {
+        for (j, &el) in row.iter().rev().enumerate() {
+            f!(P1, part1.update(inv - j, j, el, &mut part1_states[1][i]));
+            f!(P2, part2.update(inv - j, j, el, &mut counts[1][i]));
+        }
+        f!(P1, part1.clear());
+        f!(P2, part2.clear());
+    }
+
+    for (i, row) in cols.iter().enumerate() {
+        for (j, &el) in row.iter().enumerate() {
+            f!(P1, part1.update(i, j, el, &mut part1_states[2][j]));
+            f!(P2, part2.update(i, j, el, &mut counts[2][j]));
+        }
+        f!(P1, part1.clear());
+        f!(P2, part2.clear());
+    }
+
+    for (i, row) in cols.iter().enumerate() {
+        for (j, &el) in row.iter().rev().enumerate() {
+            f!(P1, part1.update(i, j, el, &mut part1_states[3][inv - j]));
+            f!(P2, part2.update(i, j, el, &mut counts[3][inv - j]));
+        }
+        f!(P1, part1.clear());
+        f!(P2, part2.clear());
+    }
+
+    if P1 {
+        let mut part1 = 0;
+        for i in 0..cols.len() {
+            part1 +=
+                (part1_states[0][i] | part1_states[1][i] | part1_states[2][i] | part1_states[3][i])
+                    .count_ones();
+        }
+        println!("Part 1: {}", part1);
+    }
+
+    if P2 {
+        let mut max = 0;
+        for i in 0..cols.len() {
+            for j in 0..cols.len() {
+                let scenic = counts[0][i][j] * counts[1][i][j] * counts[2][i][j] * counts[3][i][j];
+                if scenic > max {
+                    max = scenic;
                 }
             }
         }
 
-        el > self.max
-    }
-}
-
-type RowPair = (Row, Row);
-pub fn solve<const P1: bool, const P2: bool>(mut buf: impl BufRead) -> Option<()> {
-    let mut rows: Vec<RowPair> = Vec::new();
-    let mut cols: Vec<RowPair> = Vec::new();
-
-    let mut row_i = 0;
-    let mut col_i = 0;
-
-    let mut bytes = Vec::new();
-    buf.read_to_end(&mut bytes).ok()?;
-    rows.push(RowPair::default());
-    for &byte in &bytes {
-        // If you are still on the first row, columns need to be made
-        if row_i == 0 {
-            cols.push(RowPair::default());
-        }
-        if byte == b'\n' {
-            row_i += 1;
-            col_i = 0;
-            rows.push(RowPair::default());
-            continue;
-        }
-
-        let el = (byte - b'0') as usize + 1;
-        // Push element on the right side
-        rows[row_i].1.push(el);
-        cols[col_i].1.push(el);
-
-        col_i += 1;
-    }
-
-    row_i = 0;
-    col_i = 0;
-    let mut count = 0;
-    for &byte in &bytes {
-        if byte == b'\n' {
-            row_i += 1;
-            col_i = 0;
-            continue;
-        }
-
-        let el = (byte - b'0') as usize + 1;
-        let vis_left = rows[row_i].0.push(el);
-        let vis_right = rows[row_i].1.pop(el);
-        let vis_top = cols[col_i].0.push(el);
-        let vis_bot = cols[col_i].1.pop(el);
-
-        if vis_left || vis_right || vis_top || vis_bot {
-            count += 1;
-        }
-        col_i += 1;
-    }
-
-    if P1 {
-        println!("Part 1: {}", count);
-    }
-    if P2 {
-        println!("Part 2");
+        println!("Part 2: {}", max);
     }
     Some(())
 }
