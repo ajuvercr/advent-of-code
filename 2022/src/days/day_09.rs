4fc3948541bb90e2f6a001d720fd5834bd16839e
ajuvercr <arthur.vercruysse@ugent.be>
2022-12-09 09:37:43 +0100

    day 9 part 1

--- 2022/src/days/day_09.rs
+++ 2022/src/days/day_09.rs
@@ -1,8 +1,57 @@
-use std::io::BufRead;
+use std::{collections::HashSet, io::BufRead};
 
-pub fn solve<const P1: bool, const P2: bool>(_buf: impl BufRead) -> Option<()> {
+use crate::parser::{parse, Char};
+use derive::Parse;
+
+type Point = (isize, isize);
+
+#[derive(Parse, Debug)]
+enum Dir {
+    U,
+    L,
+    R,
+    D,
+}
+
+impl Dir {
+    fn dx(self) -> Point {
+        match self {
+            Dir::U => (0, 1),
+            Dir::L => (-1, 0),
+            Dir::R => (1, 0),
+            Dir::D => (0, -1),
+        }
+    }
+}
+
+type Line = (Dir, Char<b' '>, usize, Char<b'\n'>);
+
+pub fn solve<const P1: bool, const P2: bool>(mut buf: impl BufRead) -> Option<()> {
+    let mut bytes = Vec::new();
+    buf.read_to_end(&mut bytes).ok()?;
+    let parsed: Vec<Line> = parse(bytes)?;
+
+    let mut head = Point::default();
+    let mut tail = Point::default();
+
+    let mut locations = HashSet::new();
+    locations.insert(tail);
+
+    for (dir, _, d, _) in parsed {
+        let (dx, dy) = dir.dx();
+        for _ in 0..d {
+            head = (head.0 + dx, head.1 + dy);
+            let x = head.0 - tail.0;
+            let y = head.1 - tail.1;
+            if x.abs() > 1 || y.abs() > 1 {
+                tail = (tail.0 + x.signum(), tail.1 + y.signum());
+                locations.insert(tail);
+            }
+        }
+    }
+
     if P1 {
-        println!("Part 1");
+        println!("Part 1: {}", locations.len());
     }
     if P2 {
         println!("Part 2");
