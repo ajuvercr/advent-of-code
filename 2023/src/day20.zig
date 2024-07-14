791fdd9efab07c06933a6bb8be3152b1499a286e
ajuvercr <arthur.vercruysse@outlook.com>
2024-07-14 11:40:58 +0200

    day 20 part 2

--- 2023/src/day20.zig
+++ 2023/src/day20.zig
@@ -2,6 +2,80 @@ const std = @import("std");
 const Parser = @import("./parser.zig").Parser;
 const utils = @import("./utils.zig");
 
+fn FieldChangeN(comptime amount: usize) type {
+    return struct {
+        high: [amount]usize,
+        high_c: usize,
+        low: [amount]usize,
+        low_c: usize,
+
+        const Self = @This();
+
+        pub fn init() Self {
+            var out = Self{
+                .high = undefined,
+                .high_c = 0,
+                .low = undefined,
+                .low_c = 0,
+            };
+
+            for (0..amount) |i| {
+                out.high[i] = 0;
+                out.low[i] = 0;
+            }
+
+            return out;
+        }
+
+        fn handle(self: *Self, input: bool, iteration: usize) void {
+            if (input) {
+                if (self.high_c < amount) {
+                    self.high[self.high_c] = iteration;
+                    self.high_c += 1;
+                }
+            } else {
+                if (self.low_c < amount) {
+                    self.low[self.low_c] = iteration;
+                    self.low_c += 1;
+                }
+            }
+        }
+
+        fn done(self: *Self) bool {
+            return self.low_c == amount and self.high_c == amount;
+        }
+
+        pub fn format(
+            self: *const Self,
+            comptime fmt: []const u8,
+            options: std.fmt.FormatOptions,
+            writer: anytype,
+        ) !void {
+            _ = fmt;
+            _ = options;
+
+            try writer.print("high {}", .{self.high[0]});
+            for (1..self.high_c) |i| {
+                try writer.print(" -{}- {}", .{ self.high[i] - self.high[i - 1], self.high[i] });
+            }
+
+            try writer.print("\tlow {}", .{self.low[0]});
+            for (1..self.low_c) |i| {
+                try writer.print(" -{}- {}", .{ self.low[i] - self.low[i - 1], self.low[i] });
+            }
+        }
+
+        fn cycle(self: *const Self) usize {
+            std.debug.assert(amount >= 2);
+            return self.high[1] - self.high[0];
+        }
+    };
+}
+
+fn lcm(v1: usize, v2: usize) usize {
+    return v1 / std.math.gcd(v1, v2) * v2;
+}
+
 pub fn main() !void {
     try utils.mainImpl(day);
 }
@@ -65,7 +139,7 @@ const Module = struct {
             },
             Type.conj => {
                 const idx = self.input_idx(pulse.origin);
-                self.memory[idx] = pulse.high;
+                self.memory[idx] = pulse.high; // actually this is is_high
                 high = false;
                 for (0..self.input_c) |i| {
                     if (!self.memory[i]) {
@@ -89,10 +163,8 @@ const Module = struct {
 
 fn init_module(modules: *std.StringArrayHashMap(*Module), name: []const u8, allocator: std.mem.Allocator) !*Module {
     if (modules.get(name)) |ptr| {
-        std.debug.print("Found\n", .{});
         return ptr;
     } else {
-        std.debug.print("Not found\n", .{});
         const out = try allocator.create(Module);
         out.name = name;
         out.input_c = 0;
@@ -103,7 +175,6 @@ fn init_module(modules: *std.StringArrayHashMap(*Module), name: []const u8, allo
 }
 
 fn day(contents: []const u8, allocator: std.mem.Allocator) anyerror!void {
-    std.debug.print("Module size {}\n", .{@sizeOf(Module)});
     var par = Parser.init(contents);
     var total2: usize = 0;
 
@@ -127,38 +198,19 @@ fn day(contents: []const u8, allocator: std.mem.Allocator) anyerror!void {
         const name = par.until(' ');
 
         var module = try init_module(&modules, name, allocator);
-        std.debug.print("------- {s} -------- {} {}\n", .{ module.name, module.input_c, module.output_c });
         module.ty = ty;
         par.pos += 4;
 
         while (par.peek(0) != '\n') {
             const o = par.buf[par.pos .. par.pos + 2];
             par.pos += 2;
-            std.debug.print("Adding output {s}\n", .{o});
-
             var out_module = try init_module(&modules, o, allocator);
-            std.debug.print("Adding output {s} {*}\n", .{ out_module.name, out_module });
-
-            std.debug.print("To self {*} {} \n", .{ module, module.output_c });
 
             module.add_output(out_module);
             out_module.add_input(module);
-            std.debug.print("To out\n", .{});
 
             if (par.peek(0) == ',') par.pos += 2;
         }
-
-        // par.pos += 1;
-
-        // const o = par.until('\n');
-        // var out_module = try init_module(&modules, o, allocator);
-        // std.debug.print("Last Adding output {s}\n", .{out_module.name});
-        // std.debug.print("To self\n", .{});
-        // std.debug.print("To out\n", .{});
-        // module.output[module.input_c] = out_module;
-        // module.output_c += 1;
-        // out_module.input[out_module.input_c] = module;
-        // out_module.input_c += 1;
     }
 
     var mem_req: usize = 0;
@@ -174,24 +226,29 @@ fn day(contents: []const u8, allocator: std.mem.Allocator) anyerror!void {
         i += module.wanted_mem();
     }
 
-    const low: usize = 0;
-    const high: usize = 0;
-    var rx: usize = 0;
+    var low: usize = 0;
+    var high: usize = 0;
+
     var queue = std.TailQueue(Pulse){};
     const buffer = try allocator.alloc(u8, 500000000);
     var fba = std.heap.FixedBufferAllocator.init(buffer);
 
     const broadcaster = modules.get("broadcaster").?;
     const rx_ptr = modules.get("rx").?;
+    const gf_ptr = rx_ptr.input[0];
+
+    var gf_inputs = try allocator.alloc(FieldChangeN(2), gf_ptr.input_c);
+    for (0..gf_inputs.len) |gf_input| {
+        gf_inputs[gf_input] = FieldChangeN(2).init();
+    }
+
     @memset(mem, false);
-    while (rx != 1) {
+    var done = false;
+    while (!done) {
         fba.reset();
         const stack_alloc = fba.allocator();
-        // if (total2 == 10000) break;
-        if (total2 % 1000000 == 0)
-            std.debug.print("{}: rx {}\n ", .{ total2, rx });
+
         total2 += 1;
-        rx = 0;
         var node = try stack_alloc.create(std.TailQueue(Pulse).Node);
         node.data.target = broadcaster;
         node.data.high = false;
@@ -200,15 +257,37 @@ fn day(contents: []const u8, allocator: std.mem.Allocator) anyerror!void {
 
         while (queue.popFirst()) |first| {
             const pulse = first.data;
-            if (first.data.target == rx_ptr) {
-                if (!pulse.high)
-                    rx += 1;
+            if (pulse.high) {
+                high += 1;
             } else {
-                try first.data.target.handle(pulse, &queue, stack_alloc);
+                low += 1;
             }
+            if (first.data.target == gf_ptr) {
+                for (0..gf_inputs.len) |gf_input| {
+                    if (gf_ptr.input[gf_input] == first.data.origin) {
+                        gf_inputs[gf_input].handle(first.data.high, total2);
+                    }
+                }
+            }
+            try first.data.target.handle(pulse, &queue, stack_alloc);
+        }
+
+        done = true;
+        for (0..gf_inputs.len) |gf_input| {
+            done = done and gf_inputs[gf_input].done();
+        }
+
+        if (total2 == 1000) {
+            std.debug.print("Part1 {}\n", .{high * low});
         }
     }
 
-    std.debug.print("Part1 {}\n", .{low * high});
-    std.debug.print("Part2 {}\n", .{total2});
+    var part2: usize = 1;
+    for (0..gf_inputs.len) |gf_input| {
+        const mod = gf_inputs[gf_input];
+        std.debug.print("Input {} {s}: {}\n", .{ gf_input, gf_ptr.input[gf_input].name, mod });
+        part2 = lcm(part2, mod.cycle());
+    }
+
+    std.debug.print("Part2 {}\n", .{part2});
 }
