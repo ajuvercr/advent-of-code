7d54d7d78efa2929c8bb6f16d7ae4a08fd9a7640
ajuvercr <arthur.vercruysse@outlook.com>
2025-09-22 14:26:24 +0200

    Day 12 part 1

--- 2024/haskell/src/Day12.hs
+++ 2024/haskell/src/Day12.hs
@@ -1,98 +1,111 @@
 module Day12 (parseDay, part1, part2) where
 
 import Control.Monad.State
-import Data.Bifunctor (first)
-import Data.Functor ((<&>))
-import Data.Map (Map, insert, (!?))
-import GHC.Base (quotRemInt)
-import NanoParsec (digit, plus, runParser, spaces, star)
+import Data.Map (Map, delete, insert, map, (!), (!?))
 
-type Day = [[Int]]
+type Day = [Char]
 
-num :: Char -> Int
-num '0' = 0
-num '1' = 1
-num '2' = 2
-num '3' = 3
-num '4' = 4
-num '5' = 5
-num '6' = 6
-num '7' = 7
-num '8' = 8
-num '9' = 9
-num x = read [x]
+type Coord = (Int, Int)
+
+data Region = Region
+  { area :: Int,
+    perimeter :: Int
+  }
+  deriving (Show)
+
+data St = St
+  { look :: Map (Char, Coord) Int,
+    regions :: Map Int Region,
+    at :: Int
+  }
+  deriving (Show)
+
+new :: Region
+new = Region 0 0
+
+getKey :: Char -> Coord -> MS (Maybe Int)
+getKey i c = gets ((!? (i, c)) . look)
+
+setKey :: Char -> Coord -> Int -> MS ()
+setKey i c k = do
+  St l r a <- get
+  let nl = insert (i, c) k l
+  put $ St nl r a
+
+getRegion :: Int -> MS Region
+getRegion x = gets ((! x) . regions)
+
+newKey :: Char -> Coord -> MS Int
+newKey i c = do
+  St l r a <- get
+  let nl = insert (i, c) (a + 1) l
+  let nr = insert (a + 1) new r
+  put $ St nl nr (a + 1)
+  return (a + 1)
+
+setRegion :: Int -> Region -> MS ()
+setRegion i c = modify (\(St l r a) -> St l (insert i c r) a)
 
 parseDay :: String -> Day -- adjust type per puzzle
-parseDay = runParser parser
+parseDay x = x
+
+type MS a = State St a
+
+combineRegions :: Region -> Region -> Region
+combineRegions (Region a1 p1) (Region a2 p2) = Region (a1 + a2) (p1 + p2)
+
+combineKeys :: Int -> Int -> MS Int
+combineKeys x y = do
+  St l r a <- get
+  let l2 = Data.Map.map mapKey l
+  let r1 = r ! x
+  let r2 = r ! y
+  let nr = delete y $ insert x (combineRegions r1 r2) r
+  put $ St l2 nr a
+  return x
   where
-    parser = star (plus (digit <&> num) <* spaces)
+    mapKey i | i == y = x
+    mapKey i = i
 
--- | Split a list.
-splitHalf :: [a] -> ([a], [a])
-splitHalf xs = go xs xs
+perim :: Maybe Int -> Int
+perim (Just _) = 2
+perim _ = 0
+
+getKey' :: Char -> Coord -> Maybe Int -> Maybe Int -> MS Int
+getKey' _ _ (Just k1) (Just k2) | k1 == k2 = return k1
+getKey' _ _ (Just k1) (Just k2) = combineKeys k1 k2
+getKey' _ _ (Just k1) Nothing = return k1
+getKey' _ _ Nothing (Just k1) = return k1
+getKey' i c Nothing Nothing = newKey i c
+
+parseYard :: Coord -> Char -> MS ()
+parseYard (x, y) c = do
+  k_up <- key up
+  k_left <- key left
+  k <- getKey' c (x, y) k_up k_left
+  setKey c (x, y) k
+  Region a p <- getRegion k
+  setRegion k (Region (a + 1) (p + 4 - perim k_up - perim k_left))
   where
-    go (y : ys) (_ : _ : zs) = first (y :) (go ys zs)
-    go ys _ = ([], ys)
+    up = (x - 1, y)
+    left = (x, y - 1)
+    key = getKey c
 
-toNum :: [Int] -> Int
-toNum = foldl (\x -> (+) (10 * x)) 0
+applyYard :: Coord -> [Char] -> MS ()
+applyYard _ [] = pure ()
+applyYard (_, y) ('\n' : xs) = applyYard (0, y + 1) xs
+applyYard (x, y) (z : xs) = parseYard (x, y) z >> applyYard (x + 1, y) xs
 
-toArr :: Int -> [Int]
-toArr 0 = []
-toArr x = toArr i ++ [j]
-  where
-    (i, j) = quotRemInt x 10
-
-timesIt :: [Int] -> [Int]
-timesIt x = toArr $ 2024 * toNum x
-
-pruneZero :: [Int] -> [Int]
-pruneZero [0] = [0]
-pruneZero (0 : xs) = pruneZero xs
-pruneZero y = y
-
-type MS a = State (Map (Int, [Int]) Int) a
-
-getI :: Int -> [Int] -> MS (Maybe Int)
-getI x y = do
-  st <- get
-  let o = st !? (x, y)
-  return o
-
-putI :: Int -> [Int] -> Int -> MS Int
-putI x y o = do
-  st <- get
-  put $ insert (x, y) o st
-  return o
-
-countBlinkLength :: Int -> [Int] -> MS Int
-countBlinkLength 0 _ = pure 1
-countBlinkLength i x = do
-  m <- getI i x
-  case m of
-    Just out -> return out
-    _ -> count >>= putI i x
-  where
-    nextBlink = countBlinkLength (i - 1)
-    nextBlinks = mapM nextBlink
-    step = nextBlinks . blink
-    count = sum <$> step x
-
-blink :: [Int] -> [[Int]]
-blink [0] = [[1]]
-blink x | even (length x) = [pruneZero i, pruneZero j]
-  where
-    (i, j) = splitHalf x
-blink x = [timesIt x]
+price :: Region -> Int
+price (Region x y) = x * y
 
 -- Part 2
-part1 :: Day -> Int
-part1 day = sum o
+part1 :: [Char] -> Int
+part1 day = foldr price' 0 (regions st)
   where
-    o = evalState (mapM (countBlinkLength 25) day) mempty
+    price' r x = x + price r
+    st = execState (applyYard (0, 0) day) (St mempty mempty 0)
 
 -- Part 2
-part2 :: Day -> Int
-part2 day = sum o
-  where
-    o = evalState (mapM (countBlinkLength 75) day) mempty
+part2 :: [Char] -> Int
+part2 day = 0
